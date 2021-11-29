680ebc2c1020991d9dbb96c2749d88b248112290
Piotr Olejarz <olejarz-piotr@o2.pl>
2021-11-30 00:07:07 +0100

    creating input, welcome view of application

--- src/App.js
+++ src/App.js
@@ -3,7 +3,35 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+  }
+
+  return (
+    <section className="section-center">
+      <form className="todo-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>TODO's</h3>
+        <div className="form-control">
+          <input type="text" className="todo" placeholder="e.g Play Basketball" value={name} onChange={(e)=>setName(e.target.value)}/>
+          <button type="submit" className="submit-btn">
+            {isEditing?'edit':'submit'}
+          </button>
+        </div>
+      </form>
+      <div className="todo-container">
+        <List />
+        <button className="clear-btn">clear items</button>
+      </div>
+    </section>
+  )
 }
 
 export default App
