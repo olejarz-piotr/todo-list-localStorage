b6617e2ec15f6a316add021675b01261638a04b4
Piotr Olejarz <olejarz-piotr@o2.pl>
2021-11-30 01:03:29 +0100

    clearing list of todos, event listeners

--- src/App.js
+++ src/App.js
@@ -11,27 +11,67 @@ function App() {
   
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!name) {
+      //display alert
+
+      showAlert(true,'danger','Enter value')
+    }
+    else if (name && isEditing) {
+      //edits
+    } else {
+      //show alert 
+      showAlert(true,'success','item added')
+      const newItem = {
+        id: new Date().getTime().toString(),
+        title: name
+      }
+      setList([...list, newItem]);
+      setName('');
+    }
+  }
+
+  const showAlert = (show=false, type="",msg="") => {
+    setAlert({show,type,msg})
+  }
+
+  const clearList = () => {
+    showAlert(true, 'danger', 'empty list');
+    setList([])
+  }
+  const removeItem = (id) => {
     
+    showAlert(true, 'danger', 'item removed');
+    setList(list.filter((item) => {
+      item.id!==id
+    }))
   }
 
   return (
     <section className="section-center">
       <form className="todo-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert}/>}
         <h3>TODO's</h3>
         <div className="form-control">
-          <input type="text" className="todo" placeholder="e.g Play Basketball" value={name} onChange={(e)=>setName(e.target.value)}/>
+          <input
+            type="text"
+            className="todo"
+            placeholder="e.g Play Basketball"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
           <button type="submit" className="submit-btn">
-            {isEditing?'edit':'submit'}
+            {isEditing ? "edit" : "submit"}
           </button>
         </div>
       </form>
-      <div className="todo-container">
-        <List />
-        <button className="clear-btn">clear items</button>
-      </div>
+      {list.length > 0 && (
+        <div className="todo-container">
+          <List items={list} />
+          <button className="clear-btn" onClick={clearList}>clear items</button>
+        </div>
+      )}
     </section>
-  )
+  );
 }
 
 export default App
